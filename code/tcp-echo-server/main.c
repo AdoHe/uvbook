3912ac3eb2c7ffddc5d92da28c2c8d4796ade066
TonyAdo <coolhzb@163.com>
2014-08-21 14:29:44 +0800

    fix the tcp echo server bug....

--- code/tcp-echo-server/main.c
+++ code/tcp-echo-server/main.c
@@ -3,33 +3,36 @@
 #include <string.h>
 #include <uv.h>
 
-uv_loop_t *loop;
+#define DEFAULT_PORT 8000
 
-uv_buf_t alloc_buffer(uv_handle_t *handle, size_t suggested_size) {
-    return uv_buf_init((char*) malloc(suggested_size), suggested_size);
+uv_loop_t *loop;
+struct sockaddr_in addr;
+
+void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
+    *buf = uv_buf_init((char*) malloc(suggested_size), suggested_size);
 }
 
 void echo_write(uv_write_t *req, int status) {
     if (status == -1) {
-        fprintf(stderr, "Write error %s\n", uv_err_name(uv_last_error(loop)));
+        fprintf(stderr, "Write error %s\n", uv_strerror(status));
     }
     char *base = (char*) req->data;
     free(base);
     free(req);
 }
 
-void echo_read(uv_stream_t *client, ssize_t nread, uv_buf_t buf) {
-    if (nread == -1) {
-        if (uv_last_error(loop).code != UV_EOF)
-            fprintf(stderr, "Read error %s\n", uv_err_name(uv_last_error(loop)));
-        uv_close((uv_handle_t*) client, NULL);
-        return;
+void echo_read(uv_stream_t *client, ssize_t nread, uv_buf_t *buf) {
+    if(nread < 0) {
+        if(nread == UV_EOF) {
+            printf("End of stream\n");
+        }
+        uv_close((uv_handle_t *) client, NULL);
     }
 
     uv_write_t *req = (uv_write_t *) malloc(sizeof(uv_write_t));
-    req->data = (void*) buf.base;
-    buf.len = nread;
-    uv_write(req, client, &buf, 1, echo_write);
+    req->data = (void*) buf->base;
+    buf->len = nread;
+    uv_write(req, client, buf, 1, echo_write);
 }
 
 void on_new_connection(uv_stream_t *server, int status) {
@@ -54,11 +57,11 @@ int main() {
     uv_tcp_t server;
     uv_tcp_init(loop, &server);
 
-    struct sockaddr_in bind_addr = uv_ip4_addr("0.0.0.0", 7000);
-    uv_tcp_bind(&server, bind_addr);
+    uv_ip4_addr("0.0.0.0", DEFAULT_PORT, &addr);
+    uv_tcp_bind(&server, (const struct sockaddr *)&addr, 0);
     int r = uv_listen((uv_stream_t*) &server, 128, on_new_connection);
     if (r) {
-        fprintf(stderr, "Listen error %s\n", uv_err_name(uv_last_error(loop)));
+        fprintf(stderr, "Listen error %s\n", uv_strerror(r));
         return 1;
     }
     return uv_run(loop, UV_RUN_DEFAULT);
